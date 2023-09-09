014e75acfb5406ca27f784b24a99c0d556a02f1b
Scadaria 02 <scadaria02@iMac-de-Scadaria-2.local>
2023-09-08 23:43:02 -0300

    final

---
+++ projects/evaluate-news-nlp/src/test/formHandler.test.js
@@ -0,0 +1,6 @@
+import { handleSubmit } from "../client/js/formHandler"
+
+describe("Test the handleSubmit() function", () => {
+    test("Testing the handleSubmit() function", async () => {
+          expect(handleSubmit).toBeDefined();
+    })});
