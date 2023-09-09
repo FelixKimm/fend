f49c9c9c9b61d56e62bffbf522bd5714b9e25b19
Scadaria 02 <scadaria02@iMac-de-Scadaria-2.local>
2023-09-08 23:02:38 -0300

    before service workers

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,13 +3,27 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
+    
     Client.checkForName(formText)
 
+    const formdata = new FormData();
+    formdata.append("key", "955da75ad665f467bd061cc400fe5c55");
+    formdata.append("txt", formText);
+    formdata.append("lang", "en");
+
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+
+    fetch('https://api.meaningcloud.com/sentiment-2.1', requestOptions)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res)
+        document.getElementById('results').innerHTML = `${res.subjectivity} <br> ${res.sentence_list[0].text} <br> ${res.agreement} <br> ${res.confidence}`
     })
 }
 
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -5,12 +5,6 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-let textapi = new MeaningCloud({
-    application_key: `${process.env.API_KEY}`
-})
-
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 const app = express()
 
 app.use(express.static('dist'))
@@ -23,10 +17,14 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.get('/getKey', function(req, res) {
+    res.send({data: process.env.API_KEY})
 })
